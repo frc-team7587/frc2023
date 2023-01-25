66df9a9a5ceba92b0f0faac1e413c21a1afed830
JJ <email@example.com>
2023-01-25 17:31:51 -0500

    drivetrain with encoders

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,26 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    //drivetrain ports
+    public static final int LFront = 1;
+    public static final int LRear = 2;
+    public static final int RFront = 3;
+    public static final int RRear = 4;
+
+    //left encoder
+    public static final int[] LEncoder = new int[] {0, 1};
+    public static final boolean LReverse = false;
+
+    //right encoder
+    public static final int[] REncoder = new int[] {2, 3};
+    public static final boolean RReverse = true;
+
+    public static final int kEncoderCPR = 1024;
+    public static final double kWheelDiameterInches = 6;
+    public static final double kEncoderDistancePerPulse =
+        // Assumes the encoders are directly mounted on the wheel shafts
+        (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
+
+}
