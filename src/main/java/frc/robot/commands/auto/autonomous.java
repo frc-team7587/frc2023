ade638573b69c7cbd0a886f9cb71305ce6a87bdd
anika-k13 <94646371+anika-k13@users.noreply.github.com>
2023-03-15 15:46:44 -0400

    Update autonomous.java

--- src/main/java/frc/robot/commands/auto/autonomous.java
+++ src/main/java/frc/robot/commands/auto/autonomous.java
@@ -1,7 +1,7 @@
 package frc.robot.commands.auto;
 import frc.robot.Robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class autonomous {
